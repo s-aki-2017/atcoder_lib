017c2f77691d902520a81b89e417f9bafd66be3d
s-aki-2017 <62897463+s-aki-2017@users.noreply.github.com>
2021-07-12 20:25:45 +0900

    comment update

--- Binary_search.h
+++ Binary_search.h
@@ -1,9 +1,9 @@
 using namespace std;
 
-long binary_search(long left, long right){ //左右でTorFを分けるもの
+long binary_search(long left, long right){ //左右でTFを分けるもの
     while(right - left > 1){
         long mid = (left + right) / 2; //左寄り
-        //long mid = (left + right + 1) / 2; //右寄り
+        //long mid = (left + right + 1) / 2; //右寄り 左右でFTの場合
 
         if(1) left = mid; //
         else right = mid; //
